f5d532dd735386dd0b7b6a01a6ff3cdbcc8a08a6
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-05 23:17:29 +0530

    library

--- src/main/java/com/nest/libraryapp_backend/controller/librarycontroller.java
+++ src/main/java/com/nest/libraryapp_backend/controller/librarycontroller.java
@@ -24,5 +24,11 @@ public class librarycontroller {
         return "welcome to user login";
     }
 
+    @PostMapping("/entry")
+    public  String Bookentry()
+    {
+        return "welcome to book entry";
+    }
+
 
 }
